2b0037e9829e5fbdfd8d124a05f085b8470832f7
Ixde <camilleixde@gmail.com>
2024-07-28 16:41:11 +0200

    Cleanup + format on save (go) + code completion (#1)
    * Add .go formatting on file save
    
    * Do not notify on config change
    
    * Add none-ls and some TODOs
    
    * Reformat files to use 4spaces tabs
    
    * Add code completion and snippets

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,26 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "java", "go", "javascript", "html" },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-    })
-  end
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+        configs.setup({
+            ensure_installed = {
+                "lua",
+                "vim",
+                "vimdoc",
+                "java",
+                "go",
+                "javascript",
+                "html",
+                "css",
+                "typescript",
+                "json",
+                "sql",
+                "python",
+            },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end,
 }
