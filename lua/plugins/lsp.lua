25945e51a2dee60dce5f4bd333976bc3d954c342
Ixde <camilleixde@gmail.com>
2024-07-28 16:34:19 +0200

    Add code completion and snippets

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -18,8 +18,10 @@ return {
         config = function()
             -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
             local lspconfig = require("lspconfig")
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
             -- Lua LSP
             lspconfig.lua_ls.setup({
+                capabilities = capabilities,
                 settings = {
                     Lua = {
                         diagnostics = {
@@ -30,6 +32,7 @@ return {
             })
             -- Go LSP
             lspconfig.gopls.setup({
+                capabilities = capabilities,
                 settings = {
                     gopls = {
                         analyses = {
